48bbf4c3e44fa2996e72bff7ceec3e4de1cb45e3
Alouiscious Media <info@reneesoul.com>
2020-03-16 23:02:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+// EXAMPLE FROM LAB
+// let even = arr.filter(n => {
+//   return n % 2 === 0;
+// });
+
+
+
+function findMatching(drivers, name) {
+  return drivers.filter(searhRslt =>  
+    searhRslt.toLowerCase() === name.toLowerCase()
+   // querying the array, and returns all drivers whose names begin with the provided letters.
+  )
+}
+
+function fuzzyMatch(drivers,char)  {
+  return drivers.filter( searchRslt => 
+    searchRslt.toLowerCase().indexOf(char.toLowerCase()) === 0
+  // returns all drivers whose names begin with the provided letters
+  )
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(searchRslt =>
+    searchRslt.name === name
+    
+  )
+// return each element whose name property matches the provided string argument.  
+}
+
+
